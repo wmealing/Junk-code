2975724f8089046283291f179ff3f96729d9f418
Wade Mealing <wmealing@gmail.com>
2011-09-28 02:02:43 +1000

    This doesn't crash.

--- rust/getopts.rs
+++ rust/getopts.rs
@@ -3,21 +3,27 @@ import std::io;
 import std::vec; 
 import std::getopts;
 
-type config = {ninjas: bool, pirates: bool};
+type config = {stress: bool};
+fn parse_opts(argv: [str]) -> config {
 
+    let opts = [getopts::optflag("stress")];
+
+    let opt_args = vec::slice(argv, 1u, vec::len(argv));
+
+    alt getopts::getopts(opt_args, opts) {
+      getopts::success(m) { ret {stress: getopts::opt_present(m, "stress")} }
+      getopts::failure(_) { fail; }
+    }
+
+}
 
 fn main(args: [str]) {
 
-	let opt_args = vec::slice(args, 1u, vec::len(args));
 
-	/* Is this a comment */ 
-	let opts = [getopts::optflag("ninjas")];
+	let opts = parse_opts(args);
 
-
-	alt getopts::getopts(opt_args, opts) {
-		getopts::success(m) { let x = { ninjas: getopts::opt_present(m, "ninjas"), pirates: getopts::opt_present(m, "pirates } }
-		getopts::failure(_) { let config x = { ninjas: false, pirates: false } }
-	}
+        let out = io::stdout();
+        out.write_line(#fmt["Opts.stress is programatically set to: %b", opts.stress]);
 
 
 }
